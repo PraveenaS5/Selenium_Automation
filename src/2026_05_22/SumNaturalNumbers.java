68356b526f2b738d74a542558193b683379a1d86
PraveenaS5 <praveenasidhi@outlook.com>
2026-06-03 15:17:15 +0530

    SumNauralNumbers

---
+++ src/2026_05_22/SumNaturalNumbers.java
@@ -0,0 +1,13 @@
+public class SumNaturalNumbers {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        // Loop from 1 to 10
+        for (int i = 1; i <= 10; i++) {
+            sum = sum + i;
+        }
+
+        // Print the result
+        System.out.println("Sum of first 10 natural numbers is: " + sum);
+    }
+}
